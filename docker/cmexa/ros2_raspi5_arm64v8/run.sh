aa7836fce009f0b80a0c1e95ed2cd0a0abd13a72
cme <info@cme-robotics.com>
2025-09-11 10:57:38 +0200

    Adding more access and devices to docker run file.

--- docker/cmexa/ros2_raspi5_arm64v8/run.sh
+++ docker/cmexa/ros2_raspi5_arm64v8/run.sh
@@ -1,3 +1,11 @@
 # ros2 raspi5 cmexa Dockerfile
 
-docker run -it --rm --name cmexa-robot --network="host"  --privileged --device-cgroup-rule='c 189:* rmw' -v /dev/input/js0:/dev/input/js0 cmeresearch/cmexa:1.0
+docker run -it \
+  --rm --name cmexa-robot \
+  --network="host" \
+  --privileged \
+  --device-cgroup-rule='c 189:* rmw' \
+  -v /dev/input/js0:/dev/input/js0 \
+  --device /dev/input \
+  --group-add $(getent group input | cut -d: -f3) \
+  cmeresearch/cmexa:1.0
