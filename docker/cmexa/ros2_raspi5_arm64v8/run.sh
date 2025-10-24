0076d707f791d1375fb2cd1c43935c1d86a4e418
cme <info@cme-robotics.com>
2025-10-24 22:20:01 +0200

    Changed docker run to restart always.

--- docker/cmexa/ros2_raspi5_arm64v8/run.sh
+++ docker/cmexa/ros2_raspi5_arm64v8/run.sh
@@ -1,11 +1,13 @@
 # ros2 raspi5 cmexa Dockerfile
 
 docker run -it \
-  --rm --name cmexa-robot \
+  --name cmexa-robot \
   --network="host" \
+  --restart=always \
   --privileged \
   --ipc=host \
   -v /dev/input:/dev/input \
   --device-cgroup-rule='c 189:* rmw' \
   --group-add $(getent group input | cut -d: -f3) \
   cmeresearch/cmexa:1.0
+
