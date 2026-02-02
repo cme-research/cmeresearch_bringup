423a5e75a695cf720c3a27b75b020b92f950e0a6
cme <info@cme-robotics.com>
2026-02-02 14:22:29 +0100

    set export ROS_AUTOMATIC_DISCOVERY_RANGE=SUBNET

--- docker/cmexa/ros2_raspi5_rosjazzy_arm64v8_nav_mapping/ros_entrypoint.sh
+++ docker/cmexa/ros2_raspi5_rosjazzy_arm64v8_nav_mapping/ros_entrypoint.sh
@@ -5,7 +5,7 @@ source "/robot/ros2_ws/install/setup.bash"
 /usr/bin/brickd &
 
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-export ROS_DOMAIN_ID=0
+export ROS_DOMAIN_ID=12
 export ROS_AUTOMATIC_DISCOVERY_RANGE=SUBNET
 
 exec "$@"
