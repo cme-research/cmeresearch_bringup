1251176eafff8e5bf5e69877a6e9c411f75de519
cme <info@cme-robotics.com>
2026-03-03 22:42:47 +0100

    Updated port assignments in launch file and added robot-specific environment variables in Docker entrypoint.

--- docker/cmexaiii/ros2_raspi5_rosjazzy_arm64v8_nav_mapping/ros_entrypoint.sh
+++ docker/cmexaiii/ros2_raspi5_rosjazzy_arm64v8_nav_mapping/ros_entrypoint.sh
@@ -2,8 +2,10 @@
 
 source "/robot/ros2_ws/install/setup.bash"
 
-/usr/bin/brickd &
+#/usr/bin/brickd &
 
+export ROBOT=cmexaiii
+export ROBOT_ENV=house
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export ROS_DOMAIN_ID=12
 export ROS_AUTOMATIC_DISCOVERY_RANGE=SUBNET
